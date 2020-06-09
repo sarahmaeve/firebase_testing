539735a0c57f429ac36a30a8cfb537680b6f3874
Sarah <sarahmaeve@users.noreply.github.com>
2020-06-09 16:32:23 -0700

    final non-generalized code

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Firebase Test Problem
 Execute a series of tests on google cloud firebase
 Place the results in a top level test_result directory
 Document usage with README
+
+# INSTRUCTIONS
+
+* download and install gcloud and gsutil (should be part of the Google Cloud SDK)
+* download apk and test apk
+* create a Firebase account and project so you can use free testing cycles in firebase
+* modify testing.py script to reflect locations and settings
+* execute using `python3 testing.py`
--- testing.py
+++ testing.py
@@ -1,10 +1,17 @@
-# connect to google cloud
+"""Automate Firebase Android Tests"""
 import errno
 import os
 import re
 import subprocess
 import sys
 
+# Replace with the locations of your binaries and command paths
+# PROJECT_ID = ""  # firebase project you've made in advance
+# GCLOUD_PATH = "" # which gcloud
+# GSUTIL_PATH = "" # which gsutil
+# APK_LOCATION = "" # where your binary is
+# TEST_APK_LOCATION = "" # where your tests are
+# example locations for my run:
 PROJECT_ID = "slandroidtest"
 GCLOUD_PATH = "/Users/sarah/gcloud/google-cloud-sdk/bin/gcloud"
 GSUTIL_PATH = "/Users/sarah/gcloud/google-cloud-sdk/bin/gsutil"
@@ -13,6 +20,7 @@ TEST_APK_LOCATION = "/Users/sarah/Downloads/app-debug-androidTest.apk"
 LOCAL_OUTPUT_DIR = "./test_output"
 
 def copy_output(target_location):
+    """Copy XML output from gcloud to local directory"""
     # create location if it doesn't exist
     # standard incantation
     try:
@@ -26,8 +34,8 @@ def copy_output(target_location):
     gsutil_address = "gs://{}**.xml".format(subaddr)
 
     try:
-        subprocess.run([GSUTIL_PATH, "cp", gsutil_address, LOCAL_OUTPUT_DIR])
-        print("Copying {} ...".format(gsutil_address))
+        subprocess.run([GSUTIL_PATH, "cp", gsutil_address, LOCAL_OUTPUT_DIR], check=True)
+        print("Copying {} to {}".format(gsutil_address, LOCAL_OUTPUT_DIR))
     except subprocess.CalledProcessError:
         print("Failed to copy XML output.")
 
@@ -35,14 +43,21 @@ def output_test_data(run_output):
     """ process output from combined stdout / stderr """
     regex1 = re.compile(b"GCS bucket at \[(.+)\]") # using bytes because of stdout format
     m = re.search(regex1, run_output)
-    if m:
+    if m is not None:
         output_location = m[1].decode("utf-8") # get out of bytes
-        print("Test Output Located at: {}".format(output_location))
+        print("Full test output Located at: {}".format(output_location))
         copy_output(output_location)
+
+    # print the pretty text box with test output from STDOUT
+    regex2 = re.compile(b"\n(\+--.*$)", re.DOTALL)
+    box_match = re.search(regex2, run_output)
+    if box_match is not None:
+        print(box_match[1].decode("utf-8"))
+
     print("Test run complete.")
-    return
 
 def cloud_config():
+    """connect to your firebase project"""
     try:
         output = subprocess.run([GCLOUD_PATH, "config", "set", "project", PROJECT_ID],
                                 check=True, capture_output=True)
@@ -54,8 +69,10 @@ def cloud_config():
         print("Configuration Error: gcloud config command incorrect?")
 
 def run_tests():
+    """execute all tests then output data"""
     try:
         print("Starting test run.")
+        # could add the following : a set of subtests. However in this case, 100 / 100
         output = subprocess.run([GCLOUD_PATH, "firebase", "test", "android", "run",
                                  "--type", "instrumentation",
                                  "--app", APK_LOCATION,
@@ -69,9 +86,5 @@ def run_tests():
         print("Some tests failed.")
         output_test_data(e.stdout)
 
-# sample_output = b'\nHave questions, feedback, or issues? Get support by visiting:\n  https://firebase.google.com/support/\n\nUploading [/Users/sarah/Downloads/app-debug.apk] to Firebase Test Lab...\nUploading [/Users/sarah/Downloads/app-debug-androidTest.apk] to Firebase Test Lab...\nRaw results will be stored in your GCS bucket at [https://console.developers.google.com/storage/browser/test-lab-rur64nxyp1734-jufvw324k5q7s/2020-06-09_13:35:41.276726_nKCX/]\n\nTest [matrix-3ihaxrks9yuk1] has been created in the Google Cloud.\nFirebase Test Lab will execute your instrumentation test on 1 device(s).\nCreating individual test executions...\n................done.\n\nTest results will be streamed to [https://console.firebase.google.com/project/slandroidtest/testlab/histories/bh.9a2d958bfb99c43/matrices/7646598942614275410].\n13:38:01 Test is Pending\n13:38:50 Starting attempt 1.\n13:38:50 Started logcat recording.\n13:38:50 Started crash monitoring.\n13:38:50 Preparing device.\n13:38:50 Logging in to Google account on device.\n13:38:50 Test is Running\n13:39:02 Installing apps.\n13:39:02 Retrieving Pre-Test Package Stats information from the device.\n13:39:02 Retrieving Performance Environment information from the device.\n13:39:02 Started crash detection.\n13:39:02 Started performance monitoring.\n13:39:02 Started video recording.\n13:39:02 Starting instrumentation test.\n13:46:06 Completed instrumentation test.\n13:46:06 Stopped video recording.\n13:46:06 Stopped performance monitoring.\n13:46:06 Stopped crash monitoring.\n13:46:18 Retrieving Post-test Package Stats information from the device.\n13:46:18 Logging out of Google account on device.\n13:46:18 Stopped logcat recording.\n13:46:18 Done. Test time = 419 (secs)\n13:46:18 Starting results processing. Attempt: 1\n13:46:31 Completed results processing. Time taken = 15 (secs)\n13:46:31 Test is Finished\n\nInstrumentation testing complete.\n\nMore details are available at [https://console.firebase.google.com/project/slandroidtest/testlab/histories/bh.9a2d958bfb99c43/matrices/7646598942614275410].\n+---------+------------------------+--------------------------------+\n| OUTCOME |    TEST_AXIS_VALUE     |          TEST_DETAILS          |\n+---------+------------------------+--------------------------------+\n| Failed  | walleye-26-en-portrait | 2 test cases failed, 98 passed |\n+---------+------------------------+--------------------------------+\n'
-# output_test_data(sample_output)
-
 cloud_config()
 run_tests()
-
